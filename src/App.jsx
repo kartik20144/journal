fd250da33cab6b993e767566f4caf42baed544db
Kartik <kartikagarwal20144@gmail.com>
2024-06-19 23:04:02 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,110 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Custm from "./components/Home/Custm";
+import Hero from "./components/Home/Hero";
+import Journal from "./components/Journal/Journal";
+import SubHeader from "./components/Journal/SubHeader";
+import Author from "./components/Journal/AuthorGuidelines/Author";
+import Reviewer from "./components/Journal/ReviewerGuidelines/Reviewer";
+import Editor from "./components/Journal/EditorGuidelines/Editor"
+import Heroimage from "./components/Journal/Heroimage";
+import ReHero from "./components/Journal/ReHero";
+import Submitbtn from "./components/Submitbtn";
+import Register from "./components/Register";
+import Testimonial from "./components/Testimonial";
+import JoinUsBox from "./components/Home/JoinUsBox";
+import JournalGrid from "./components/Home/JournalGrid";
+
+// import Custm from "./Components/Home/Custm";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <>
+          <Header />
+          <Hero />
+          <Custm />
+          <JoinUsBox />
+          <JournalGrid />
+          <Testimonial />
+          <Footer />
+        </>
+      ),
+    },
+    {
+      path: "/journal",
+      element: (
+        <>
+          <Header />
+          <ReHero />
+          <SubHeader />
+          <Journal />
+          <Footer />
+        </>
+      ),
+    },
+    {
+      path: "/journal/author",
+      element: (
+        <>
+          <Header />
+          <ReHero />
+          <Author />
+          <Submitbtn />
+          <Footer />
+        </>
+      ),
+    },
+    {
+      path: "/journal/reviewer",
+      element: (
+        <>
+          <Header />
+          <ReHero />
+          <Reviewer />
+          <Submitbtn />
+          <Footer />
+        </>
+      ),
+    },
+    {
+      path: "/journal/editor",
+      element: (
+        <>
+          <Header />
+          <ReHero />
+          <Editor />
+          <Submitbtn />
+          <Footer />
+        </>
+      ),
+    },
+    {
+      path: "/register",
+      element: (
+        <>
+          <Header />
+          <Register />
+          <Footer />
+        </>
+      ),
+    },
+  ]);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <RouterProvider router={router} />
+      {/* <Header />
+      <Hero />
+      <Custm />
+      <Journal />
+      <Footer /> */}
+      {/* <Custm /> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
