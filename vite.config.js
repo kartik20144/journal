2d7841d4ad3aec30d4079dfea42d6d9338bc0cd3
Kartik <kartikagarwal20144@gmail.com>
2024-07-05 00:20:22 +0530

    bug

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
 //   base: "/journal/",
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Output directory for production build
+    assetsDir: '', // Relative directory for static assets in the build output
+    minify: 'terser', // Minification strategy (terser or esbuild)
+    sourcemap: false, // Generate source maps for production build
+  },
+
+  optimizeDeps: {
+    include: ['react', 'react-dom'], // Dependencies to include in bundle optimization
+  },
 })
